6c0cc13e1bb0358828e7c1d15729b933ced6a21a
pwnflakes <pwnflakes25@gmail.com>
2020-06-18 20:08:16 +0300

    Added 4 sections to homepage, added testimonial module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestimonialComponent } from './homePage/testimonial/testimonial.component';
+import { HomeComponent } from './homePage/home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestimonialComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/homePage/testimonial/testimonial.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
+
+@Component({
+  selector: 'app-testimonial',
+  templateUrl: './testimonial.component.html',
+  styleUrls: ['./testimonial.component.css']
+})
+export class TestimonialComponent implements OnInit {
+
+testimonials = [
+  {
+    name: 'Mr Jojo',
+    message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    imageUrl: 'https://randomuser.me/api/portraits/men/47.jpg'
+  },
+  {
+    name: 'Mr Rabi',
+    message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    imageUrl: 'https://randomuser.me/api/portraits/men/47.jpg'
+  },
+  {
+    name: 'Ms Celine',
+    message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    imageUrl: 'https://randomuser.me/api/portraits/men/47.jpg'
+  }
+];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    $(document).ready(() => {
+    $('.carousel').carousel();
+   });
+  }
+
+  trackByFn(index, item) {
+   return index;
+  }
+
+}
