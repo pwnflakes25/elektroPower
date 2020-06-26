79ddda08faeb7c06f874f8dd0902ff5830d00058
pwnflakes <pwnflakes25@gmail.com>
2020-06-26 16:11:17 +0300

    Added package select option in quote form

--- src/app/homePage/quote-form/quote-form.component.ts
+++ src/app/homePage/quote-form/quote-form.component.ts
@@ -1,26 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { FormGroup, FormControl, Validators  } from '@angular/forms';
 import {MailerService} from '../../packages/mailer.service';
+declare const M;
 
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.scss']
 })
-export class QuoteFormComponent implements OnInit {
+export class QuoteFormComponent implements OnInit, AfterViewInit{
 quoteForm: FormGroup;
 
 
   constructor(private mailerService: MailerService) { }
 
   ngOnInit(): void {
-
     this.quoteForm = new FormGroup({
     name: new FormControl('', Validators.required),
     email: new FormControl('', Validators.required),
     phone: new FormControl('', Validators.required),
+    package: new FormControl('', Validators.required)
   });
 
+  }
+
+  ngAfterViewInit(): void {
+      var elems = document.querySelectorAll('select');
+      var instances = M.FormSelect.init(elems);
   }
 
   onSubmit() {
