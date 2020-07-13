5b49aecf0d556f322b260451b24df6c852d3060a
pwnflakes <pwnflakes25@gmail.com>
2020-07-13 17:05:48 +0300

    scrapped bad practice in the calculator component, added an export array of appliances, changed calculation method, design tweak for calculator section

---
+++ src/app/calculator/applianceArray.ts
@@ -0,0 +1,107 @@
+export const appliancesList = [
+   {
+     name: 'Tube Lights',
+     power: 40,
+     quantity: 0
+   },
+   {
+     name: 'Energy Savers',
+     power: 40,
+     quantity: 0
+   },
+   {
+     name: 'Tube Lights',
+     power: 25,
+     quantity: 0
+   },
+   {
+     name: 'LED Bulbs',
+     power: 7,
+     quantity: 0
+   },
+   {
+     name: 'Fans',
+     power: 70,
+     quantity: 0
+   },
+   {
+     name: 'TVs',
+     power: 139,
+     quantity: 0
+   },
+   {
+     name: 'LED TVs',
+     power: 130,
+     quantity: 0
+   },
+   {
+     name: 'Washing Machines',
+     power: 500,
+     quantity: 0
+   },
+   {
+     name: 'Refrigerators',
+     power: 250,
+     quantity: 0
+   },
+   {
+     name: 'Computers',
+     power: 100,
+     quantity: 0
+   },
+   {
+     name: 'Water Pumps',
+     power: 300,
+     quantity: 0
+   },
+   {
+     name: 'irons',
+     power: 1000,
+     quantity: 0
+   },
+   {
+     name: 'Washing Machines',
+     power: 500,
+     quantity: 0
+   },
+   {
+     name: 'Split AC 1 Ton',
+     power: 1090,
+     quantity: 0
+   },
+   {
+     name: 'Split AC 1.5 Ton',
+     power: 1560,
+     quantity: 0
+   },
+   {
+     name: 'Split AC 2 Ton',
+     power: 1930,
+     quantity: 0
+   },
+   {
+     name: 'Split AC 3.5 Ton',
+     power: 3500,
+     quantity: 0
+   },
+   {
+     name: 'Split AC 4 Ton',
+     power: 4000,
+     quantity: 0
+   },
+   {
+     name: 'Inverter AC 1 Ton',
+     power: 726,
+     quantity: 0
+   },
+   {
+     name: 'Inverter AC 1.5 Ton',
+     power: 1077,
+     quantity: 0
+   },
+   {
+     name: 'Inverter AC 2 Ton',
+     power: 1344,
+     quantity: 0
+   },
+ ]
--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import {RecommendationComponent} from './recommendation/recommendation.component';
-import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import {MatDialog, MatDialogRef} from '@angular/material/dialog';
+import {appliancesList} from './applianceArray';
 declare const M;
 
 @Component({
@@ -9,56 +9,31 @@ declare const M;
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.scss']
 })
-export class CalculatorComponent implements OnInit {
-calculatorForm: FormGroup;
-appliances = [
-  'tubeLights', 'energySavers', 'ledBulbs',
-  'fans', 'tvs', 'ledTvs', 'washingMachines',
-   'freezers', 'refrigerators', 'computers', 'waterPumps',
-   'irons', 'splitAc1', 'splitAc15', 'splitAc2', 'splitAc35',
-   'splitAc4', 'inverterAc1', 'inverterAc15', 'inverterAc2'
- ]
+export class CalculatorComponent implements OnInit, AfterViewInit {
+appliances = [...appliancesList];
+isTouched = false;
 
-  constructor(private fb: FormBuilder, public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
-    this.calculatorForm = this.fb.group({
-      tubeLights: [0, Validators.required],
-      energySavers: [0, Validators.required],
-      ledBulbs: [0, Validators.required],
-      fans: [0, Validators.required],
-      tvs: [0, Validators.required],
-      ledTvs: [0, Validators.required],
-      washingMachines: [0, Validators.required],
-      freezers: [0, Validators.required],
-      refrigerators: [0, Validators.required],
-      computers: [0, Validators.required],
-      waterPumps: [0, Validators.required],
-      irons: [0, Validators.required],
-      splitAc1: [0, Validators.required],
-      splitAc15: [0, Validators.required],
-      splitAc2: [0, Validators.required],
-      splitAc35: [0, Validators.required],
-      splitAc4: [0, Validators.required],
-      inverterAc1: [0, Validators.required],
-      inverterAc15: [0, Validators.required],
-      inverterAc2: [0, Validators.required],
-    })
   }
 
   ngAfterViewInit(): void {
     let elems  = document.querySelectorAll("input[type=range]");
     let instance = M.Range.init(elems);
+
+    //below is just a code to change background color the thumb of the slider (number that pops up)
+    let list = document.getElementsByClassName('thumb');
+    for (let i = 0; i <= list.length; i++) {
+      (<HTMLElement>list[i]).style.backgroundColor = "#232330";
+    }
   }
 
-  //I know this is a bad practice, gotta update it. client wants it done real quickly
   onCalculate() {
-    let value = this.calculatorForm.value;
-    let sum = value.tubeLights * 40 + value.energySavers * 25 + value.ledBulbs * 7 + value.tvs * 250 +
-      value.ledTvs * 50 + value.washingMachines * 800 + value.freezers * 350 + value.refrigerators * 350 +
-      value.computers * 250 + value.waterPumps * 700 + value.irons * 1000 + value.splitAc1 * 1800 + value.splitAc15 * 2400 +
-      value.splitAc2 * 3000 + value.splitAc35 * 7500 + value.splitAc4 * 8000 + value.inverterAc1 * 1000 +
-      value.inverterAc15 * 1500 + value.inverterAc2 * 2000;
+    let sum = 0;
+    this.appliances.forEach(element => {
+      sum += (element.quantity * element.power);
+    });
     this.openDialog(sum);
   }
 
@@ -70,6 +45,13 @@ appliances = [
       dialogRef.afterClosed().subscribe(result => {
        console.log('The dialog was closed');
      });
+  }
+
+  onReset() {
+    this.appliances.forEach(element => {
+      element.quantity = 0;
+    })
+    this.isTouched = false;
   }
 
 }
